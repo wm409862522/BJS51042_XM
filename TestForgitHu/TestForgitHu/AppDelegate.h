61b878b15611ca03de799b80fd04fd26f1121cd9
小明 <409862522@qq.com>
2016-01-14 10:13:00 +0800

    第一次提交 版本1.0

---
+++ TestForgitHu/TestForgitHu/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TestForgitHu
+//
+//  Created by 小明 on 16/1/14.
+//  Copyright © 2016年 小明. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TestForgitHu/TestForgitHu/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TestForgitHu
+//
+//  Created by 小明 on 16/1/14.
+//  Copyright © 2016年 小明. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
